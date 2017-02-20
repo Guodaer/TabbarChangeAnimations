61dacb81b24bb4629de1edcb90f0c5681b680141
guoda <1023251767@qq.com>
2017-02-20 10:23:54 +0800

    Initial Commit

---
+++ TabbarChangeAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabbarChangeAnimations
+//
+//  Created by X-Designer on 17/2/20.
+//  Copyright © 2017年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabbarChangeAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabbarChangeAnimations
+//
+//  Created by X-Designer on 17/2/20.
+//  Copyright © 2017年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
